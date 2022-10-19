09d4211459de0edbad11a2c3f457467731f8e647
caitlinrwAND <caitlin.whiteley@and.digital>
2022-10-19 10:26:33 +0100

    first css updates

--- src/App.js
+++ src/App.js
@@ -3,28 +3,32 @@ import "./App.css";
 import Form from "./Form";
 
 function App() {
-  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="Page-title">Title of page</p>
+        <p className="Page-title">
+          <span className="Red-text">AND</span>title
+        </p>
+        <div className="App-logo" />
       </header>
-        <div className="Form-container">
-          <div>
-            <p>
-              Description: Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-              dolor in reprehenderit in voluptate velit esse cillum dolore eu
-              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est
-              laborum.
-            </p>
-          </div>
-          <Form />
+      <div className="Form-container">
+        <div>
+          <p>
+            ANDtitle is a platform that allows you to communicate your ANDtitle
+            with other ANDies. The platform facilitates communication in a
+            simple manner. By inserting the relevant information, the functions
+            will enable ANDies to communicate their title with each other via
+            email. Instructions: input the email address of the ANDi initiating
+            the communication and a clue to enable the ANDies you are
+            communicating with to have a guess on what your ANDtitle is. If the
+            ANDi you are communicating with does not guess your ANDtitle within
+            5 minutes, we will be sending them and email automatically with the
+            answer.
+          </p>
         </div>
+        <Form />
+      </div>
     </div>
   );
 }
